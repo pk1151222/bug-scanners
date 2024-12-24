a92f3e1187751a1b83d422276143ed9dc85a27d3
pk1151222 <73013821+pk1151222@users.noreply.github.com>
2024-12-24 20:32:20 +0530

    Update save.go

--- output/save.go
+++ output/save.go
@@ -1,28 +1,10 @@
 package output
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
+    "fmt"
 )
 
-// SaveResults saves the subdomain and TLS details in JSON format
-func SaveResults(subdomains []string, tlsDetails *TLSDetails) error {
-	file, err := os.Create("results.json")
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	results := map[string]interface{}{
-		"subdomains":    subdomains,
-		"tls_details": tlsDetails,
-	}
-
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-	if err := encoder.Encode(results); err != nil {
-		return err
-	}
-	return nil
+func SaveResults(results interface{}) {
+    // Logic to save results to file (JSON, CSV, etc.)
+    fmt.Println("Saving results:", results)
 }
