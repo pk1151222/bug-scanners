471e67f3f14946d19ddac55abb825fd47e5a44ce
pk1151222 <73013821+pk1151222@users.noreply.github.com>
2024-12-24 18:40:39 +0530

    Create cve_checker.go

---
+++ pkg/cve_scanner/cve_checker.go
@@ -0,0 +1,9 @@
+package cve_scanner
+
+import "fmt"
+
+func CheckForCVE(domain string) {
+	// Simulate CVE scanning
+	fmt.Printf("Checking CVEs for %s...\n", domain)
+	// Add CVE checking logic here
+}
