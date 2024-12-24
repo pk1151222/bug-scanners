3f877ebf8d3e445820a8f566662e4af51464056e
pk1151222 <73013821+pk1151222@users.noreply.github.com>
2024-12-24 20:25:18 +0530

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,20 +1,20 @@
 package cmd
 
 import (
-	"fmt"
-	"github.com/spf13/cobra"
+    "fmt"
+    "github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "bug-scanner",
-	Short: "A tool to scan for subdomains and security bugs",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Bug Scanner Tool")
-	},
+    Use:   "bugscanner",
+    Short: "A tool for scanning web vulnerabilities",
+    Run: func(cmd *cobra.Command, args []string) {
+        fmt.Println("Bug Scanner is running")
+    },
 }
 
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-	}
+    if err := rootCmd.Execute(); err != nil {
+        fmt.Println(err)
+    }
 }
